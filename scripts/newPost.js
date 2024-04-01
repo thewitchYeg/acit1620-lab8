31e970c328c9ce3950813d19d445226407956c25
yegane <yeganeh.sn@gmail.com>
2024-04-01 15:52:18 -0700

    edit

---
+++ scripts/newPost.js
@@ -0,0 +1,37 @@
+
+async function addPost(post) {
+       /**
+     * 1. Get the list posts from local storage
+     *      if local storage is empty, initialize the list of posts to an empty array
+     * 2. Add the new post at the front of list
+     * 3. Save the list posts back to local storage
+     */
+
+    // Your code here
+  
+  try {
+      
+      let posts = JSON.parse(localStorage.getItem('posts')) || [];
+
+      
+      posts.unshift(post);
+
+      
+      localStorage.setItem('posts', JSON.stringify(posts));
+      
+      
+      window.location.href = 'index.html'; 
+  } catch (error) {
+      console.error('Error adding post:', error.message);
+  }
+}
+
+
+
+document.querySelector('#new-post').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+    addPost({title, body});
+    e.target.reset();
+});
